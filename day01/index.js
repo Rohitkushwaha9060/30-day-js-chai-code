a4aceebf850f53870f6f92ef1c92f592531ecfb7
Rohit Kushwaha <rajdilkhush0987654321@gmail.com>
2024-07-13 20:37:57 +0530

    day one complete

---
+++ day01/index.js
@@ -0,0 +1,72 @@
+//  Activity 1: Variables Declaration
+var num = 10;
+// console.log(num);
+
+let name = "Rohit";
+// console.log(name);
+
+// Activity 2: Constants Declaration
+const isTrue = true;
+// console.log(isTrue);
+
+// Activity 3: Data Types
+let num1 = 10;
+
+// console.log(num1 + " num1 type is " + typeof num1);
+
+let name1 = "Rohit";
+// console.log(name1 + " name1 type is " + typeof name1);
+
+const isTrue1 = true;
+// console.log(isTrue1 + " isTrue1 type is " + typeof isTrue1);
+
+const profile = {
+  name: "Rohit",
+  age: 25,
+  isStudent: true,
+  hobbies: ["coding", "playing", "reading"],
+};
+
+// console.log(profile + " profile type is " + typeof profile);
+
+const users = ["Rohit", "Ankit", "Rahul"];
+
+// console.log(users + " users type is " + typeof users);
+
+// Activity 4: Reassignment of Variables
+let num2 = 10;
+// console.log(num2);
+num2 = 20;
+// console.log(num2);
+
+// Activity 5: Understanding Constants
+
+// const isTrue2 = true;
+// console.log(isTrue2);
+// isTrue2 = false;
+// console.log(isTrue2);
+
+// Features  : Variables type console
+
+const MyBool = true;
+const MyString = "Rohit";
+const MyNumber = 10;
+const MyArray = [1, 2, 3];
+const MyObject = {
+  name: "Rohit",
+  age: 25,
+  isStudent: true,
+};
+
+// console.log("Boolean" + " MyBool type is " + typeof MyBool);
+// console.log("String" + " MyString type is " + typeof MyString);
+// console.log("Number" + " MyNumber type is " + typeof MyNumber);
+// console.log("Array" + " MyArray type is " + typeof MyArray);
+// console.log("Object" + " MyObject type is " + typeof MyObject);
+
+// Features  : Reassignment Demonstration
+
+let num3 = 10;
+console.log(num3);
+num3 = 20;
+console.log(num3);
