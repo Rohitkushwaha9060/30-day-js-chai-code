ac4f8b8973b26c3541b9a14faf52471715df3e81
Rohit Kushwaha <rajdilkhush0987654321@gmail.com>
2024-07-16 16:48:50 +0530

    day 04 complete

---
+++ day04/index.js
@@ -0,0 +1,88 @@
+// ! Activity: 01 For Loop
+
+// Task :01
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// Task :02
+// for (let i = 1; i <= 10; i++) {
+//   console.log(5 * i);
+// }
+
+// ! Activity: 02 While Loop
+
+// Task :03
+// let sum = 0;
+// let i = 1;
+
+// while (i <= 10) {
+//   sum += i;
+//   i++;
+// }
+
+// console.log(sum);
+
+// Task :04
+// let i = 10;
+// while (i >= 1) {
+//   console.log(i);
+//   i--;
+// }
+
+// ! Activity: 03 Do While Loop
+
+// Task :05
+// let i = 1;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 5);
+
+// Task :06
+// function factorial(n) {
+//   let result = 1;
+//   let i = 1;
+
+//   do {
+//     result *= i;
+//     i++;
+//   } while (i <= n);
+
+//   return result;x
+// }
+
+// console.log(factorial(5));
+
+// ! Activity: 04 Nested Loops
+
+// Task :07
+// let count = 5;
+// for (let i = 1; i <= count; i++) {
+//   let star = "";
+//   for (let j = 0; j < i; j++) {
+//     star += "* ";
+//   }
+//   console.log(star);
+// }
+
+// ! Activity: 05 Loop Control Statements
+
+// Task :08
+// let i = 0;
+// while (++i <= 10) {
+//   if (i === 5) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// Task :09
+// let i = 0;
+// while (++i <= 10) {
+//   if (i === 7) {
+//     break;
+//   }
+//   console.log(i);
+// }
